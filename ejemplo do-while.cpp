d6e0a45027d8363deb65863140fcfd3437284b60
Gutiérrez Cesar <138553082+cesar-g1@users.noreply.github.com>
2025-06-20 02:04:34 -0300

    eprimer ejemplo

---
+++ ejemplo do-while.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	int contador=0;
+	float nota,suma=0,promedio;
+	
+	do{
+		printf("Ingrese la nota del estudiante %d: ",contador+1);
+		scanf("%f",&nota);
+		
+		suma+=nota;
+		contador++;
+		
+	} while(contador<10);
+	promedio=suma/10;
+	printf("El promedio de las notas es: %.2f\n",promedio);
+	
+	return 0;
+}
+
